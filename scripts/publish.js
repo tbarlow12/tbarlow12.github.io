37199293f067ca564a5f0d394290a6345199b084
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-18 08:34:00 -0700

    remove silent

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,7 +2,6 @@ const ghpages = require("gh-pages");
 
 ghpages.publish("build", {
   repo: "https://" + process.env.GITHUB_TOKEN + "@github.com/tbarlow12/personal-website-beta.git",
-  silent: true  
 },(err) => {
   if (err) {
     console.error(err);
