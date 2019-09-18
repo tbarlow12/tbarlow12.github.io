9bb4ee22fb907113c699e02f1b273b9a26aa9d2b
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-18 09:16:14 -0700

    test

--- src/react/pages/homePage.tsx
+++ src/react/pages/homePage.tsx
@@ -18,7 +18,7 @@ export function HomePage() {
 
   return (
     <div>
-      This is my home page
+      This is my home page test
       {/* {pulls && pulls.map((pull: any) => <div>{pull.title}</div>)}
       {events && events.map((event: any) => <div>{event.type}</div>)} */}
     </div>
