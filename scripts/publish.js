94c80fe3a3467f13d3ad2226926f52a4332e09aa
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-18 06:53:04 -0700

    Add remote in publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,9 +1,12 @@
 const ghpages = require("gh-pages");
 
-ghpages.publish("build", (err) => {
+ghpages.publish("build", {
+  repo: "https://" + process.env.GITHUB_TOKEN + "@github.com/tbarlow12/personal-website-beta.git",
+  silent: true  
+},(err) => {
   if (err) {
     console.error(err);
   } else {
     console.log("Deployed successfully");
   }
-})
+});
