384ffedfa00bb7ed051425d07ad798619bd9cc7c
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-18 08:38:30 -0700

    update publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,11 @@
 const ghpages = require("gh-pages");
 
 ghpages.publish("build", {
+  user: {
+    name: "Tanner Barlow",
+    email: "tanner.barlow.dev@gmail.com"
+  },
+  message: "Auto-generated commit to publish GitHub page",
   repo: "https://" + process.env.GITHUB_TOKEN + "@github.com/tbarlow12/personal-website-beta.git",
 },(err) => {
   if (err) {
