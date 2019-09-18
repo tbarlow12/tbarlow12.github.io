4e606d82354509a5e6aa11253104454b211d1485
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-18 06:13:10 -0700

    Update name

--- src/react/pages/HomePage.tsx
+++ src/react/pages/HomePage.tsx
@@ -8,20 +8,20 @@ export interface HomePageState {
 
 export function HomePage() {
 
-  const [pulls, setPulls] = useState();
-  const [events, setEvents] = useState();
+  // const [pulls, setPulls] = useState();
+  // const [events, setEvents] = useState();
 
-  const githubService = new GitHubService();
-  githubService.pulls("serverless", "serverless-azure-functions")
-    .then((pullResult) => setPulls(pullResult));
-  githubService.userEvents("tbarlow12")
-    .then((eventsResult) => setEvents(eventsResult))
+  // const githubService = new GitHubService();
+  // githubService.pulls("serverless", "serverless-azure-functions")
+  //   .then((pullResult) => setPulls(pullResult));
+  // githubService.userEvents("tbarlow12")
+  //   .then((eventsResult) => setEvents(eventsResult))
 
   return (
     <div>
       This is my home page
-      {pulls && pulls.map((pull: any) => <div>{pull.title}</div>)}
-      {events && events.map((event: any) => <div>{event.type}</div>)}
+      {/* {pulls && pulls.map((pull: any) => <div>{pull.title}</div>)}
+      {events && events.map((event: any) => <div>{event.type}</div>)} */}
     </div>
   )
 }
