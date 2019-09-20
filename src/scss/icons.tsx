b7b716cf7d0bce3429501911c71b193f3bb84d89
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-20 14:57:04 -0700

    add more icons

--- src/scss/icons.tsx
+++ src/scss/icons.tsx
@@ -2,5 +2,11 @@ export const icons = {
   home: "fas fa-home",
   blog: "fas fa-keyboard",
   github: "fab fa-github",
-  linkedin: "fab fa-linkedin"
+  linkedin: "fab fa-linkedin",
+  resume: "fas fa-file-alt",
+  tasks: "fas fa-tasks",
+  code: "fas fa-code-branch",
+  education: "fas fa-user-graduate",
+  basketball: "fas fa-basketball-ball",
+  volunteer: "fas fa-hands-helping",
 }
