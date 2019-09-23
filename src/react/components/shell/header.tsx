442b61902e9f1ecfcddf90b25a0d5bd5090a06f3
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-23 08:11:16 -0700

    remove pic from header

--- src/react/components/shell/header.tsx
+++ src/react/components/shell/header.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export function Header() {
   return (
     <div className="app-main-header">
-      <div className="icon">
+      {/* <div className="icon">
         <a href="/">
           <img src="/resources/images/TannerPic.jpg" alt=""/>
         </a>
-      </div>
+      </div> */}
       <div className="app-main-header-title">
         <h1>tanner barlow</h1>
       </div>
