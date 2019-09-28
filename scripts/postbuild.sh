515a0ffc33c378f226964a56de7a59a2f21959f9
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-28 16:15:16 -0700

    add post build script

---
+++ scripts/postbuild.sh
@@ -0,0 +1,9 @@
+rm -rf docs
+mv build docs
+echo "tannerbarlow.com" > docs/CNAME
+git config --global user.name "Tanner Barlow"
+git config --global user.email "tanner.barlow.dev@gmail.com"
+git remote add authOrigin "https://${GITHUB_ACCESS_TOKEN}@github.com/tbarlow12/tbarlow12.github.io.git",
+git add .
+git commit -m "Auto-generated build commit"
+git push authOrigin master
