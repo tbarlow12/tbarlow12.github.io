931d580576c652022482b0df39f9f12741db0c01
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-30 13:57:07 -0700

    publish to master

---
+++ scripts/publish.js
@@ -0,0 +1,19 @@
+const ghpages = require("gh-pages");
+
+ghpages.publish("build", {
+  user: {
+    name: "Tanner Barlow",
+    email: "tanner.barlow.dev@gmail.com"
+  },
+  message: "Auto-generated commit to publish GitHub page",
+  repo: "https://" + process.env.GITHUB_ACCESS_TOKEN + "@github.com/tbarlow12/tbarlow12.github.io.git",
+  branch: "master",
+  silent: true
+},(err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    console.log("Deployed successfully");
+  }
+});
