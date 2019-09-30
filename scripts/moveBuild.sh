b20b2abf8f9205042c0c8fdf584ca09c941ae494
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-09-30 07:33:42 -0700

    rename scripts

--- scripts/moveBuild.sh
+++
@@ -1,2 +0,0 @@
-rm -rf precache-manifest.*.js asset-manifest.json index.html service-worker.js tanner_icon.ico tanner.jpg manifest.json static
-cp -r ./build/* ./
---
+++ scripts/preBuild.sh
@@ -0,0 +1,2 @@
+rm -rf precache-manifest.*.js asset-manifest.json index.html service-worker.js tanner_icon.ico tanner.jpg manifest.json static ./build
+npm run markdown
