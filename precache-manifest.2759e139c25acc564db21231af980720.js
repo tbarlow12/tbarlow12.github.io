d618d7262988354cb33370ad6060d7735e21c039
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-10-15 12:06:52 +0000

    Auto-generated commit to publish GitHub page

--- precache-manifest.2759e139c25acc564db21231af980720.js
+++ precache-manifest.2759e139c25acc564db21231af980720.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3811d6ad46fa1a85e0c21995bf12d43d",
+    "revision": "6ef858bf24df4b71e7c5d608092fcb2a",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.e515a07b.chunk.css"
   },
   {
-    "revision": "47e99880f28c459a4573",
+    "revision": "75100340e183130920c3",
     "url": "/static/css/main.e97b265c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.33bd5724.chunk.js"
   },
   {
-    "revision": "47e99880f28c459a4573",
-    "url": "/static/js/main.76dd2d9d.chunk.js"
+    "revision": "75100340e183130920c3",
+    "url": "/static/js/main.a6c0dcbb.chunk.js"
   },
   {
     "revision": "f6a2a72b5dedea610859",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ff658261ad68bf561c4e84325ecf9d1.js"
+  "/precache-manifest.2759e139c25acc564db21231af980720.js"
 );
 
 self.addEventListener('message', (event) => {
