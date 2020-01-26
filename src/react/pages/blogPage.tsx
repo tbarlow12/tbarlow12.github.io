95774b011b0e10271ad709ca166c150704b5a2e9
Tanner Barlow <tanner.barlow.dev@gmail.com>
2020-01-25 22:48:11 -0700

    Fix styles

--- src/react/pages/blogPage.tsx
+++ src/react/pages/blogPage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BlogService } from "../../services/blogService";
 import { BlogPostPreview } from "../components/blog/blogPostPreview";
+import { BlogPostMetadata } from "../../models/blog";
 
 export function BlogPage() {
 
   return (
     <div className="blog-page">
       <h1>Blog</h1>
-      {BlogService.getPreviews().map((blogPost) => 
+      {BlogService.getPreviews().map((blogPost: BlogPostMetadata) => 
         <div className="blog-page-preview">
           {BlogPostPreview(blogPost)}
         </div>)}
