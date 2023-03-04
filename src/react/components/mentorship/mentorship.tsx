f746b43b91475bf3523e4ffc6d34a607cf42878b
Tanner Barlow <tanner.barlow.dev@gmail.com>
2023-03-04 14:44:35 -0700

    Add mentorship page

---
+++ src/react/components/mentorship/mentorship.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function MentorshipPage() {
+  // Paste the stripe-pricing-table snippet in your React component
+  return (
+    <stripe-pricing-table 
+      pricing-table-id="prctbl_1MhEWLC2YtCMdaoBwilcEzXJ"
+      publishable-key="pk_live_51Me8O9C2YtCMdaoBRLYwgg7lXZ8NTM9w8TwtzSBpzXkAoiA627DrUqq9BXEonOn8pORBqYH2IkQONGWSxFYO2QJ0007VJjGi26"
+    >
+    </stripe-pricing-table>
+  );
+}
+
+export default MentorshipPage;
--- src/scss/icons.tsx
+++ src/scss/icons.tsx
@@ -14,5 +14,6 @@ export const icons = {
   moon: "fas fa-moon",
   trash: "far fa-trash-alt",
   book: "fas fa-book",
+  mentorship: "fas fa-users",
   none: "",
 }
